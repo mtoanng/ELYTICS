434867f8e5d60b89318dbc78c81d64f62417ca2f
Henry Wright <wrh1tb@bosch.com>
2026-03-24 16:35:26 +0100

    fixes

--- frontend/spaces/sherlock/management/track_record.py
+++ frontend/spaces/sherlock/management/track_record.py
@@ -4,6 +4,6 @@ from services.backend_service import get_table_as_df
 register_page(__name__, path="/sherlock/management/track-record", title="HOLMES - Sherlock - Track Record")
 
 def track_record_layout():
-    return html.Div(f"welcome to Track Record home")
+    return html.Div(f"welcome to Track Record home - disabled")
 
 layout = track_record_layout
