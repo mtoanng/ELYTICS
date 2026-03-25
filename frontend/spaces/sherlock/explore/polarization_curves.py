75ad9c6b970bbc1ee98c354457ab7ae95ce676a9
dmg1tb <dmg1tb@bosch.com>
2026-03-25 09:21:52 +0000

    fixed certificates

--- frontend/spaces/sherlock/explore/polarization_curves.py
+++ frontend/spaces/sherlock/explore/polarization_curves.py
@@ -1,10 +1,16 @@
 from dash import html, register_page
 from services.backend_service import get_table_as_df
 
-register_page(__name__, path="/sherlock/data-exploration/polarization-curves", title="HOLMES - Sherlock - Polarization Curves")
+register_page(
+    __name__,
+    path="/sherlock/data-exploration/polarization-curves",
+    title="HOLMES - Sherlock - Polarization Curves",
+)
+
 
 def polarization_curves_layout():
-    data = get_table_as_df('sherlock', 'order_overview')
+    data = get_table_as_df("sherlock", "order_overview")
     return html.Div(f"welcome to polarization-curves home, latest data: {data}")
+
 
 layout = polarization_curves_layout
