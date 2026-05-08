78685dfc33583f9537f134d8eff9ca6289f9e26e
Henry Wright <wrh1tb@bosch.com>
2026-05-08 09:36:18 +0200

    Switched to dash-leaflet and removed table

--- backend/config/enola.py
+++ backend/config/enola.py
@@ -1,6 +1,12 @@
 from backend.internal.config_types import MetadataConfig, TabularConfig, TimeseriesConfig
 
+_USER = ["IdM2BCD_holmes_pemely_user"]
+_TABULAR_TTL = 3600
+
 # Enola routes require management group: IdM2BCD_holmes_pemely_management
-TABULAR_CONFIG: list[TabularConfig] = []
+TABULAR_CONFIG: list[TabularConfig] = [
+    TabularConfig(table_name="customer", route_name="customer", auth_groups=_USER, ttl=_TABULAR_TTL),
+]
+
 TIMESERIES_CONFIG: list[TimeseriesConfig] = []
 METADATA_CONFIG: list[MetadataConfig] = []
